ddced2d4e750b3023295da1a0a93005a215afa04
Efren Garcia Guarneros <efren.garcia.guarneros@gmail.com>
2020-04-10 16:03:55 -0500

    Se agregan validaciones al modelo de CourseForCreationDto

--- Models/CourseForCreationDto.cs
+++ Models/CourseForCreationDto.cs
@@ -1,10 +1,25 @@
-using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PluralsightCourseAPI.Models
 {
-    public class CourseForCreationDto
+    public class CourseForCreationDto : IValidatableObject
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
+
+        [MaxLength(1500)]
         public string Description { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == Description)
+            {
+                yield return new ValidationResult(
+                    "The provided description should be different from title.",
+                    new[] { "Request" });
+            }
+        }
     }
 }
